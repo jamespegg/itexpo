2e6730dd40b62f11e36262fe331a2c62f0e1158d
James Pegg <jpegg@PC05035.hsa.co.uk>
2016-01-20 13:03:03 +0000

    Added ngCloak, added content dropdowns

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -1,5 +1,5 @@
 app.controller('HomeController', ['$scope', '$rootScope', function($scope, $rootScope){
-    $rootScope.title = 'IT Expo 2015';
+    $rootScope.title = 'IT Expo 2016';
     $rootScope.back = false;
     $rootScope.navbarColour = "";
 }]);
