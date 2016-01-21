9d00261b9f9c9ca014fb026208c287bb76ab3648
James Pegg <jpegg@PC05035.hsa.co.uk>
2016-01-21 14:04:04 +0000

    Scroll to bottom of the page when opening the last description

--- js/events.js
+++ js/events.js
@@ -5,3 +5,8 @@ $(document).on('show.bs.collapse', '.agenda-item', function(){
 $(document).on('hide.bs.collapse', '.agenda-item' , function(){
     $(this).prev().find('.fa').removeClass('fa-flip-vertical');
 });
+
+// Scroll to the bottom of the page when the last description is opened
+$(document).on('shown.bs.collapse', '.list-group-item:last-child .agenda-item', function(){
+    window.scrollTo(0,document.body.scrollHeight);
+});
